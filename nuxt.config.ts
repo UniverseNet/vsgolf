7d1e927bd8e5d27ffb8d8e3d49856f945ca96772
joowave <joowave@naver.com>
2026-06-10 13:49:58 +0900

    Chore: Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,30 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-06-09',
+  compatibilityDate: "2025-06-09",
   ssr: false,
   experimental: {
     viteEnvironmentApi: true,
   },
   nitro: {
-    preset: 'static',
+    preset: "static",
     output: {
-      dir: 'docs',
-      publicDir: 'docs',
+      dir: "docs",
+      publicDir: "docs",
     },
   },
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   components: [
     {
-      path: '~/components/bet-board',
+      path: "~/components/bet-board",
       pathPrefix: false,
     },
   ],
-  css: ['~/assets/css/base.css'],
+  css: ["~/assets/css/base.css"],
   app: {
+    baseURL: "/vsgolf/",
     head: {
-      htmlAttrs: { lang: 'ko' },
-      title: '스크린골프 저녁내기 진행판',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        { name: 'theme-color', content: '#101a17' },
-      ],
+      htmlAttrs: {lang: "ko"},
+      title: "스크린골프 저녁내기 진행판",
+      meta: [{charset: "utf-8"}, {name: "viewport", content: "width=device-width, initial-scale=1.0"}, {name: "theme-color", content: "#101a17"}],
     },
   },
-})
+});
